792690522c45a62375e0a9b28c99c179e5554342
Leandro <leandrojimenezdev@gmail.comm>
2024-11-08 01:04:11 -0500

    Update Firebase configuration to use environment variables for improved security

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 export const app = initializeApp({
-  apiKey: "AIzaSyAKyIIfYr2Gx1JyLAmYhb0g66PYX9mz9c8",
-  authDomain: "nextchatfirebase.firebaseapp.com",
-  projectId: "nextchatfirebase",
-  storageBucket: "nextchatfirebase.firebasestorage.app",
-  messagingSenderId: "651068465170",
-  appId: "1:651068465170:web:98df27b305369cb04d38cb",
-  measurementId: "G-0ZJ9LQMCKW",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 });
 
 export const auth = getAuth(app);
